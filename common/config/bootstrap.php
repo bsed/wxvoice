b496e6dd99ac22b667721ccb3e0fc7b371cf2765
yanli <896792616@qq.com>
2017-09-13 19:11:06 +0800

    delete ignore files

--- common/config/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-Yii::setAlias('@common', dirname(__DIR__));
-Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
-Yii::setAlias('@mobile', dirname(dirname(__DIR__)) . '/mobile');
-Yii::setAlias('@public', dirname(dirname(__DIR__)) . '/public/attachment');
-
